fe58049c5163a0e0e777b2bef3055ebd80c09bdd
Lotz <lukas.lotz@cgi.com>
2021-12-14 07:41:30 +0100

    state day14

--- 14/1.py
+++ 14/1.py
@@ -1,6 +1,15 @@
 import os
+import re
+from collections import Counter
 
 l = [x for x in open('test_in.txt').read().split('\n')]
 str = l[0]
-for x in l[1:]:
-	print(x)
+for f in range(10):
+	for x in l[2:]:
+		strarr=[p for p in str]
+		m=x.split(',')
+		g=[o.start() for o in re.finditer(m[0],str)]
+		for i in g:
+			strarr.insert(i+1,m[1])
+		str="".join(strarr)
+print(Counter(str))
